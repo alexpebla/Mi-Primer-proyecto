619d17629d5e37cacb0d6318e83a8a8199270675
Alexander P <97420693+alexpebla@users.noreply.github.com>
2022-04-05 20:09:07 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-let saludo = " Hola mundo";
-console.log(saludo);
+const express = require('express')
+const app = express()
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, () => {
+  console.log('Open')
+})
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/viws/home.html')
+});
